8386a90e17d4f54df8673700d177ebadeb1fdf37
DaidipyaMathur <daidipya_m@cs.iitr.ac.in>
2025-04-24 16:01:52 +0530

    Update Footer.js

--- client/src/compenents/FooterFAB/Footer/Footer.js
+++ client/src/compenents/FooterFAB/Footer/Footer.js
@@ -1,16 +1,27 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import { GitHub, Instagram, LinkedIn, Mail } from '@mui/icons-material';
 import { Box } from '@mui/material';
 import classes from './Footer.module.css';
 
 const Footer = () => {
-
-    const clickHandler = (link, name) => {
+    const clickHandler = (link) => {
         window.open(link, '_blank');
     }
 
     return (
-        <></>
+        <footer className={classes.footer}>
+            <Box className={classes.container}>
+                <div className={classes.social}>
+                    <GitHub 
+                        className={classes.icon} 
+                        onClick={() => clickHandler('https://github.com/Whizzion957/OJ')} 
+                    />
+                </div>
+                <div className={classes.copyright}>
+                    Made with ❤️ by Aadit, Ayushka, Daidipya, Devansh, Drishti and Rohan
+                </div>
+            </Box>
+        </footer>
     )
 }
 
